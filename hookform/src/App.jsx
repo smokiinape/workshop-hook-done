39741dfc971fe7eac064d41fc404b776e9ac35b4
smokiinape <114114045+smokiinape@users.noreply.github.com>
2023-05-16 13:05:11 +0200

    first commit

---
+++ hookform/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './Form.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hook Forms</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
